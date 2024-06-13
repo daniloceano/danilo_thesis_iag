5fe0eae2914e852aead898a7d7061196803b320c
daniloceano <danilo.oceano@gmail.com>
2024-06-13 19:03:21 -0300

    Maps - track densities updated

--- src_chapter_4/map_track_density.py
+++ src_chapter_4/map_track_density.py
@@ -6,7 +6,7 @@
 #    By: daniloceano <danilo.oceano@gmail.com>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/13 09:22:31 by daniloceano       #+#    #+#              #
-#    Updated: 2024/06/13 11:06:58 by daniloceano      ###   ########.fr        #
+#    Updated: 2024/06/13 19:03:06 by daniloceano      ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,32 +17,63 @@ import matplotlib.colors as mcolors
 import numpy as np
 import os
 import matplotlib as mpl
+import matplotlib.ticker as mticker
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 
 # Configuration
+# INFILES_DIRECTORY = '/Users/danilocoutodesouza/Documents/Programs_and_scripts/SWSA-cyclones_energetic-analysis/periods_species_statistics/70W-no-continental/track_density'
+INFILES_DIRECTORY = '/home/daniloceano/Documents/Programs_and_scripts/SWSA-cyclones_energetic-analysis/periods_species_statistics/70W-no-continental/track_density'
+OUTPUT_DIRECTORY = '../figures_chapter_4/track_density/'
 PHASES = ['incipient', 'intensification', 'mature', 'decay',
           'intensification 2', 'mature 2', 'decay 2', 'residual']
 REGIONS = [False, "ARG", "LA-PLATA", "SE-BR"]
 SEASONS = ['DJF', 'JJA']
 LABELS = ['(A)', '(B)', '(C)', '(D)', '(E)', '(F)', '(G)', '(H)']
 COLORS = ['#AFC4DA', '#4471B2', '#B1DFA3', '#EFF9A6', '#FEEC9F', '#FDB567', '#F06744', '#C1274A']
-INFILES_DIRECTORY = '/Users/danilocoutodesouza/Documents/Programs_and_scripts/SWSA-cyclones_energetic-analysis/periods_species_statistics/70W-no-continental/track_density'
-OUTPUT_DIRECTORY = '../figures_chapter_4/track_density/'
+
 datacrs = ccrs.PlateCarree()
 proj = ccrs.AlbersEqualArea(central_longitude=-30, central_latitude=-35, standard_parallels=(-20.0, -60.0))
 
-
 # Utility Functions
 def gridlines(ax):
-    gl = ax.gridlines(draw_labels=True, zorder=2, linestyle='dashed', alpha=0.8, color='#383838')
+    # Create the gridlines with a finer interval
+    gl = ax.gridlines(draw_labels=True, zorder=2, linestyle='dashed',
+                      alpha=0.6, color='#383838', linewidth=0.5)
+    gl.xlocator = mticker.FixedLocator(np.arange(-180, 181, 10))
+    gl.ylocator = mticker.FixedLocator(np.arange(-90, 91, 10))
+    
+    # Use formatters to show labels
+    gl.xformatter = LongitudeFormatter()
+    gl.yformatter = LatitudeFormatter()
+    
+    # Control which labels are displayed
     gl.xlabel_style = {'size': 14, 'color': '#383838'}
     gl.ylabel_style = {'size': 14, 'color': '#383838'}
-    gl.bottom_labels = False
-    gl.right_labels = False
-    gl.top_labels = True
-    gl.left_labels = True
     gl.xlabel_style = {'rotation': 0, 'ha': 'center', 'fontsize': 12}
     gl.ylabel_style = {'rotation': 0, 'ha': 'center', 'fontsize': 12}
+    gl.bottom_labels = False
+    gl.right_labels = False
+    gl.top_labels = False
+    gl.left_labels = False
 
+    # Coarser interval
+    gl2 = ax.gridlines(draw_labels=True, zorder=2, linestyle='dashed', alpha=0, color='#383838')
+    gl2.xlocator = mticker.FixedLocator(np.arange(-180, 181, 20))
+    gl2.ylocator = mticker.FixedLocator(np.arange(-90, 91, 20))
+    
+    # Use formatters to show labels
+    gl2.xformatter = LongitudeFormatter()
+    gl2.yformatter = LatitudeFormatter()
+    
+    # Control which labels are displayed
+    gl2.xlabel_style = {'size': 14, 'color': '#383838'}
+    gl2.ylabel_style = {'size': 14, 'color': '#383838'}
+    gl2.xlabel_style = {'rotation': 0, 'ha': 'center', 'fontsize': 12}
+    gl2.ylabel_style = {'rotation': 0, 'ha': 'center', 'fontsize': 12}
+    gl2.bottom_labels = False
+    gl2.right_labels = False
+    gl2.top_labels = True
+    gl2.left_labels = True
 
 def plot_density(fig, ax, phase, density, label):
     # ax.set_extent([-90, 180, 0, -90], crs=datacrs)
@@ -51,10 +82,10 @@ def plot_density(fig, ax, phase, density, label):
 
     levels_dict = {
         'incipient': [0.1, 1, 2, 3, 5, 8, 10, 13, 15, 18, 20, 22, 25, 30, 35],
-        'intensification': [0.1, 1, 2, 5, 8, 10, 15, 20, 30, 40, 50, 70, 100, 130],
-        'decay': [0.1, 1, 2, 5, 8, 10, 15, 20, 30, 40, 50, 70, 100, 130],
-        'mature': [0.1, 1, 2, 3, 5, 8, 10, 13, 15, 18, 20, 25, 30, 35, 40],
-        'default': [0.1, 1, 2, 3, 5, 8, 10, 13, 15, 18, 20, 22, 25, 28, 30]
+        'intensification': [0.1, 1, 2, 5, 8, 10, 15, 20, 30, 40, 50, 60, 80, 100],
+        'mature': [0.1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20],
+        'decay': [0.1, 1, 2, 3, 5, 8, 10, 13, 15, 18, 20, 22, 25, 30, 35],
+        'default': [0.1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20]
     }
     levels = levels_dict.get(phase, levels_dict['default'])
 
@@ -127,7 +158,7 @@ def plot_each_phase():
 def plot_secondary_development():
     secondary_phases = ['intensification 2', 'mature 2', 'decay 2', 'residual']
     for season in SEASONS:
-        fig = plt.figure(figsize=(12, 5))
+        fig = plt.figure(figsize=(12, 7))
         for i, phase in enumerate(secondary_phases):
             ax = fig.add_subplot(2, 2, i+1, projection=proj)
 
@@ -149,7 +180,7 @@ def plot_secondary_development():
                 colorbar = plt.colorbar(cf, cax=cbar_axes, ticks=ticks, format='%g', orientation='horizontal')
                 colorbar.ax.tick_params(labelsize=12)
 
-        plt.subplots_adjust(wspace=0.15, hspace=-0.15)
+        plt.subplots_adjust(wspace=0.15)
         fname = os.path.join(OUTPUT_DIRECTORY, f'density_map_secondary_development{season_str}.png')
         plt.savefig(fname, bbox_inches='tight')
         plt.close(fig)
